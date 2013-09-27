e947e40d54f3ca20915895edc8c87895f5ce1289
Nicholas Weatherburn <n.weatherburn@gmail.com>
2013-09-27 12:26:07 +1200

    Fix generalout issues, add button to add new contact

--- src/nwea171/softeng206/contacts/ContactListActivity.java
+++ src/nwea171/softeng206/contacts/ContactListActivity.java
@@ -2,12 +2,16 @@ package nwea171.softeng206.contacts;
 
 import java.util.ArrayList;
 
+import nwea171.softeng206.contacts.swipetodismiss.SwipeDismissListViewTouchListener;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -33,7 +37,9 @@ public class ContactListActivity extends Activity {
 
 		contactListView = (ListView) findViewById(R.id.contact_listview);
 		setupListView();
-		
+
+
+
 	}
 
 	// Instantiates the values in the list view.
@@ -42,8 +48,8 @@ public class ContactListActivity extends Activity {
 		for (int i = 0; i < 10; i++) {
 			contacts.add(new Contact());
 		}
-		
 
+		// Adds onItemCickListener to the list of contacts
 		listAdapter = new ContactListAdapter();
 		contactListView.setAdapter(listAdapter);
 		contactListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -57,12 +63,55 @@ public class ContactListActivity extends Activity {
 
 		});
 
+		// The following code in this method, is taken from a open source Github.
+		// https://github.com/romannurik/android-swipetodismiss
+
+		// Create a ListView-specific touch listener. ListViews are given special treatment because
+		// by default they handle touches for their list items... i.e. they're in charge of drawing
+		// the pressed state (the list selector), handling list item clicks, etc.
+		SwipeDismissListViewTouchListener touchListener =
+				new SwipeDismissListViewTouchListener(
+						contactListView,
+						new SwipeDismissListViewTouchListener.DismissCallbacks() {
+							@Override
+							public boolean canDismiss(int position) {
+								return true;
+							}
+
+							@Override
+							public void onDismiss(ListView listView, int[] reverseSortedPositions) {
+								for (int position : reverseSortedPositions) {
+									((ContactListAdapter) listAdapter).remove((Contact) listAdapter.getItem(position));
+								}
+								((ContactListAdapter) listAdapter).notifyDataSetChanged();
+							}
+						});
+		contactListView.setOnTouchListener(touchListener);
+		// Setting this scroll listener is required to ensure that during ListView scrolling,
+		// we don't look for swipes.
+		contactListView.setOnScrollListener(touchListener.makeScrollListener());
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.contact_list, menu);
+		MenuInflater inflater=getMenuInflater();
+		inflater.inflate(R.menu.contact_list, menu);
+
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.add_new_contact:
+			Intent intent = new Intent();
+			intent.setClass(ContactListActivity.this, EditContactActivity.class);
+			startActivity(intent);
+		}
 		return true;
 	}
 
---
+++ src/nwea171/softeng206/contacts/ContactViewActivity.java
@@ -0,0 +1,39 @@
+package nwea171.softeng206.contacts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ContactViewActivity extends Activity {
+	
+	Contact contact;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contact_view);
+		
+		// Add contact information to Views;
+		contact = (Contact) getIntent().getSerializableExtra("contact"); 
+		((TextView) findViewById(R.id.contact_first_name)).setText(contact.getFirstName());
+		((TextView) findViewById(R.id.contact_surname)).setText(contact.getSurname());
+		((TextView) findViewById(R.id.contact_date_of_birth)).setText(contact.getDateOfBirth());
+		((TextView) findViewById(R.id.contact_mobile_number)).setText(contact.getMobileNumber());
+		((TextView) findViewById(R.id.contact_home_number)).setText(contact.getHomeNumber());
+		((TextView) findViewById(R.id.contact_work_number)).setText(contact.getWorkNumber());
+		((TextView) findViewById(R.id.contact_email)).setText(contact.getEmailAddress());
+		((TextView) findViewById(R.id.contact_address)).setText(contact.getAddress());
+		((TextView) findViewById(R.id.contact_notes)).setText(contact.getNotes());
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.contact_view, menu);
+		return true;
+	}
+
+}
